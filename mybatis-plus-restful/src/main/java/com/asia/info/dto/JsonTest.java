8cda65fe8b4410727181f74c881a4b96c401cfac
wangbin13 <wangbin13@asiainfo.com>
2020-07-06 15:59:54 +0800

    新增jsonTest文件

---
+++ mybatis-plus-restful/src/main/java/com/asia/info/dto/JsonTest.java
@@ -0,0 +1,11 @@
+package com.asia.info.dto;
+
+/**
+ * @ClassName JsonTest
+ * @Description TODO
+ * @Author wangbin
+ * @Date 2020/7/6 15:59
+ */
+public class JsonTest {
+    private String name;
+}
